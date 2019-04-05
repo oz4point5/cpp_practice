f1b09ebe701d901049a580125eca0fa4055e9118
oz4point5 <trapezohedron0000@gmail.com>
2019-04-05 21:50:24 +0900

    Add files via upload

---
+++ atCoderPractice/APG4B_1.12.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  int ans = 1;
+
+  // ここにプログラムを追記
+  for(int i = 0;i < S.size(); i++){
+    if(i % 2 == 0){
+      continue;
+    }else if(S.at(i) == '+'){
+      ans ++;
+    }else if(S.at(i) == '-'){
+      ans --;
+    }
+  }
+
+  cout << ans << endl;
+
+}
